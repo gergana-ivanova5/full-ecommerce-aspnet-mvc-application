1043410fd18a01c7f71fd62baf78087e154608b7
Geri <gerganaivanova015@gmail.com>
2022-09-12 23:42:53 +0300

    Actros Controller added.

---
+++ movieTickets/Controllers/ActrosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movieTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieTickets.Controllers
+{
+    public class ActrosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActrosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
