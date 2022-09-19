49f7ad0d7847b2bb0ec38457a74e7d39357ecdf3
Geri <gerganaivanova015@gmail.com>
2022-09-19 20:38:37 +0300

    Designing the Movie card done.

--- movieTickets/Controllers/MoviesController.cs
+++ movieTickets/Controllers/MoviesController.cs
@@ -19,7 +19,7 @@ namespace movieTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.Include(n => n.Cinema).ToListAsync();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(allMovies);
         }
     }
